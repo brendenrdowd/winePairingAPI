28f7202df9afae9c206d2fecb424fc7a57f57b00
brendenrdowd <brenden.dowd@yahoo.com>
2019-11-16 14:22:01 -0800

    added API call

--- logic.js
+++ logic.js
@@ -1,23 +1,35 @@
 const searchUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?food="
 
 
-function getPairing(query){
+function getPairing(query) {
     // brenden
-    fetch(searchUrl + query)
-.then(response => {
-	console.log(response);
-})
-.then(response => displayResults(response))
-.catch(err => {
-	console.log(err);
-});
+    const url = searchUrl + query;
+
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "fT7bfv7YGGmshOWpHRupMoc5eXzLp1mUWeEjsn9TtTgMefEoGe"
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log('ok')
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
 }
 
-function displayResults(responseJson){
+function displayResults(responseJson) {
     // oleg
 }
 
-function watchForm(){
+function watchForm() {
     // brenden
 }
 
